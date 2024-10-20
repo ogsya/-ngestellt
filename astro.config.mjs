5b72fe38287b32d5c4c806da55173c8a8ed7a459
Siyar Gülmez <siyargulmez@Mac.fritz.box>
2024-10-20 11:30:46 +0200

    Switch to static site generation (SSG)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from "@astrojs/tailwind";
 // Import the Vercel adapter
 import vercel from '@astrojs/vercel/serverless';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
+  adapter: netlify(),
 });
