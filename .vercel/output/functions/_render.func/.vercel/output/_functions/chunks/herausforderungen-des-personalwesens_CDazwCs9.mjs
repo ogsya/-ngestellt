c6efb43d07ac81e87f87093a98ec261ab4daeef5
ogsya <siyar@angestellt.org>
2024-10-23 21:06:41 +0200

    w

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/herausforderungen-des-personalwesens_CDazwCs9.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./herausforderungen-des-personalwesens_oRJ_kjxw.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
